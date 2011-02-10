02c8702b1edf5831378cb622e3454909e0af4783
ImprobabilityMan <ImprobabilityMan@gmail.com>
2011-02-09 18:51:10 -0800

    added solution

--- 12.clj
+++ 12.clj
@@ -6,5 +6,25 @@
 	(let [new-val (+ (inc (- b a)) b)] 
 	  (lazy-seq (cons a (trn b new-val))))) 1 3))
 
-(defn largest-prime-divisor [num]
-  (
+(defn one? [n] (= n 1))
+
+(defn count-factors [f]
+  (let [counts (group-by identity f)]
+    (reduce * (map #(inc (count %)) (map val counts)))))
+
+(defn count-divisors [num]
+  (loop [factors '()
+         n num
+         prime primes]
+    (let [p (first prime)]
+      (cond
+       (one? n) (count-factors factors)
+       (zero? (mod n p)) (recur (conj factors p) (/ n p) primes)
+       :default (recur factors n (next prime))))))
+
+(defn euler-12 [count]
+  (loop [tri triangle-numbers]
+    (let [t (first tri)]
+      (if (> (count-divisors t) count)
+        t
+        (recur (next tri))))))
